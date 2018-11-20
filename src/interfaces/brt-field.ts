6c0551057824748fdbd7ed155f6e068f66678063
louis amstutz <louis.amstutz@moventes.com>
2018-11-20 15:24:15 +0100

    add string on brtfieldtype

--- src/interfaces/brt-field.ts
+++ src/interfaces/brt-field.ts
@@ -3,7 +3,7 @@ import { BrtFieldType } from './../enums/brt-field-type';
 export interface BrtField {
   id?: string;
   label: string;
-  type: BrtFieldType;
+  type: BrtFieldType | 'checkbox' | 'textarea' | 'email' | 'text';
   required?: boolean;
   value?: any;
 }
