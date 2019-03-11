d879373ae1081d4878dd155bd9a9007b592be37e
louis amstutz <louis.amstutz@moventes.com>
2019-03-11 15:06:53 +0100

    0.2.21

--- src/interfaces/brt-feedback.ts
+++ src/interfaces/brt-feedback.ts
@@ -1,14 +1,14 @@
+import { BrtCookies } from "./brt-cookies";
 import { BrtData } from "./brt-data";
+import { BrtLog } from "./brt-log";
 import { BrtNavigatorInfo } from "./brt-navigator-info";
 import { BrtScreenInfo } from "./brt-screen-info";
-import { BrtCookies } from "./brt-cookies";
-import { BrtLog } from "./brt-log";
 import { BrtServiceWorker } from "./brt-service-worker";
 
 export interface BrtFeedback {
   date: string;
   apiKey?: string;
-  canvas?: string;
+  canvas?: string | Blob;
   url?: string;
   cookies?: BrtCookies;
   navigator?: BrtNavigatorInfo;
