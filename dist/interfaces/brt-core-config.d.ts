9d256424e127ecdd9786c686e59d50102fe5e0fd
gwenaelMoventes <33447961+gwenaelMoventes@users.noreply.github.com>
2019-03-26 10:31:13 +0100

    Update brt-core-config.d.ts

--- dist/interfaces/brt-core-config.d.ts
+++ dist/interfaces/brt-core-config.d.ts
@@ -1,5 +1,5 @@
 import { BrtLogCacheLength } from './brt-log-cache-length';
 export interface BrtCoreConfig {
     logCacheLength?: BrtLogCacheLength;
-    addGetParamsToLog?: boolean;
+    addQueryParamsToLog?: boolean;
 }
