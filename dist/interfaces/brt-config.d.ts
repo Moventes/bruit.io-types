6a39072939059a4797abb238209e5d4d2c36b4d7
Louis AMSTUTZ <louis.moventes@gmail.com>
2019-07-09 11:28:11 +0200

    0.3.1

--- dist/interfaces/brt-config.d.ts
+++ dist/interfaces/brt-config.d.ts
@@ -2,13 +2,11 @@ import { BrtColors } from './brt-colors';
 import { BrtField } from './brt-field';
 import { BrtLabels } from './brt-labels';
 export interface BrtConfig {
-    apiKey?: string;
     form: Array<BrtField>;
     labels?: BrtLabels;
     colors?: BrtColors;
     closeModalOnSubmit?: boolean;
     durationBeforeClosing?: number;
-    apiUrl?: string;
     elementToRenderSelector?: string;
     screenshot?: {
         maxWidth?: number;
--- dist/interfaces/brt-core-config.d.ts
+++ dist/interfaces/brt-core-config.d.ts
@@ -1,5 +1,7 @@
 import { BrtLogCacheLength } from './brt-log-cache-length';
 export interface BrtCoreConfig {
+    apiKey?: string;
+    apiUrl?: string;
     logCacheLength?: BrtLogCacheLength;
     addQueryParamsToLog?: boolean;
 }
