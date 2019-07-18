9c540ac2fc01fa3de4c09bd37cf80095273dcf19
louis amstutz <louis.amstutz@moventes.com>
2019-07-18 10:28:23 +0200

    0.3.2-2

--- dist/index.js
+++ dist/index.js
@@ -3,14 +3,14 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./enums/brt-connection-type"));
+__export(require("./enums/brt-effective-type"));
 __export(require("./enums/brt-field-type"));
 __export(require("./enums/brt-http-request"));
 __export(require("./enums/brt-key-type"));
-__export(require("./enums/brt-log-type"));
 __export(require("./enums/brt-log-http-type"));
-__export(require("./enums/brt-tool-id"));
-__export(require("./enums/brt-tool-type"));
+__export(require("./enums/brt-log-type"));
 __export(require("./enums/brt-permission-name"));
 __export(require("./enums/brt-permission-status"));
-__export(require("./enums/brt-effective-type"));
-__export(require("./enums/brt-connection-type"));
+__export(require("./enums/brt-tool-id"));
+__export(require("./enums/brt-tool-type"));
---
+++ dist/interfaces/brt-log-config.d.ts
@@ -0,0 +1,5 @@
+import { BrtLogCacheLength } from './brt-log-cache-length';
+export interface BrtLogConfig {
+    logCacheLength?: BrtLogCacheLength;
+    addQueryParamsToLog?: boolean;
+}
